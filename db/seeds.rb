60a3221d5d4b30b63649c08f8ea4c2cc41a0e13c
Patrick <pdn6991@gmail.com>
2021-06-15 20:38:07 -0400

    Added faker, added items through seed.rb, and rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: Faker::Commerce.product_name, price: Faker::Commerce.price, description: Faker::Marketing.buzzwords)
+product.save
