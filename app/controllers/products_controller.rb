852c4d513a103146b65370d53ff1a74e66ac9450
Patrick <pdn6991@gmail.com>
2021-06-17 20:28:04 -0400

    Deleted old routes, added index and show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
-  def get_one_product
-    product = Product.last
-    render json: product.as_json
-  end
-
-  def get_all_products
+  def index
     products = Product.all
     render json: products.as_json
+  end
+
+  def show
+    the_id = params[:id]
+    product = Product.find_by(id: the_id)
+    render json: product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/one_product", controller: "products", action: "get_one_product"
-  get "/all_products", controller: "products", action: "get_all_products"
+  get "/products", controller: "products", action: "index" 
+  get "/products/:id", controller: "products", action: "show" 
 end
